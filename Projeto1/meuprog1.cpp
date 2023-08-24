58c37928d2d3b8ff1f4710128c33af4090712b62
ticihoog <ticianapaternostro@gmail.com>
2023-08-24 17:39:02 -0300

    aula 03

---
+++ Projeto1/meuprog1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+
+string nome;
+cout << "Digite seu nome: ";
+cin >> nome;
+cout << "Bom dia, "<< nome << ".";
+cout << endl;
+
+    return 0;
+}
+
+//sem erros
---
+++ Projeto1/meuprog2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+int a = 0, b = 0;
+cout << "Digite um numero: ";
+cin >> a;
+cout << "Digite outro número: ";
+cin >> b;
+cout << "Os valores digitados foram " << a << " e " << b << endl;
+cout << "A soma dos valores é: " << a+b << endl;
+cout << "A subtração dos valores é: " << a-b << endl;
+cout << "A multiplcação dos valores é: " << a*b << endl;
+cout << "A divisão dos valores é: " << a/b << endl;
+cout << endl;
+
+    return 0;
+}
